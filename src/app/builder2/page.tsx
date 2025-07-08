56f073b872aff18ce31c2abe7a3b8235c3ee3cdc
sakibrimon <mdsakibhassanrimon@gmail.com>
2025-07-08 21:38:28 +0600

    first commit

---
+++ src/app/builder2/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect } from "react";
+
+const Builder = () => {
+  useEffect(() => {
+    import("grapesjs").then((grapesjs) => {
+      const editor = grapesjs.init({
+        container: "#gjs", // Container for the editor
+        fromElement: true, // Allow dragging from the element
+        height: "100vh",
+        width: "auto",
+        storageManager: {
+          type: "local", // Store the design in local storage
+        },
+        blockManager: {
+          appendTo: "#blocks", // Where blocks will be displayed
+          blocks: [
+            {
+              id: "text-block",
+              label: "Text Block",
+              content: '<div class="p-4 bg-gray-300">Hello, GrapesJS!</div>',
+              category: "Basic",
+            },
+          ],
+        },
+      });
+    });
+  }, []);
+
+  return (
+    <div className="flex">
+      <div id="blocks" className="w-1/4 bg-gray-200 p-4">
+        <button className="bg-blue-500 text-white p-2">Add Text Block</button>
+      </div>
+      <div id="gjs" className="w-3/4 bg-white"></div>
+    </div>
+  );
+};
+
+export default Builder;
---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+// src/global.d.ts
+declare module 'grapesjs' {
+  const grapesjs: any;
+  export = grapesjs;
+}
